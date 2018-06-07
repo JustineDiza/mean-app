01f0e1c5f85de080fecfd95e2848cb58eae2c46e
JustineDiza <justinediza@gmail.com>
2018-06-07 18:41:59 +0800

    First commit - Finished simple course on Nodejs and Expressjs

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var routes = require('./api/routes');
+var bodyParser = require('body-parser');
+
+var port_id = 3000;
+
+// Middleware
+
+app.use(function(req, res, next){
+	console.log(req.method, req.url);
+	next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// *****
+
+// Only need strings and arrays, not other data types
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/api', routes);
+
+var server = app.listen(port_id, function () {
+	var port = server.address().port;
+	console.log('MEAN app has started on: ' + port);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# MEAN stack app training
+
+This is just a training ground for me to test out my knowledge and apply things I've learned from a Udemy course about creating MEAN stack web app.
